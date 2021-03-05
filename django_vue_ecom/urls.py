ed84eae80f5d3b1adcad2386eda7188d3aa7bf53
Marius Sabaliauskas <rha2r4qef9@privaterelay.appleid.com>
2021-03-05 11:11:35 +0100

    [ADD] api.py, cart functionality

--- django_vue_ecom/urls.py
+++ django_vue_ecom/urls.py
@@ -7,12 +7,21 @@ from apps.store.views import product_detail, category_detail
 
 from apps.cart.views import cart_detail
 
+from apps.store.api import api_add_to_cart
+
 urlpatterns = [
     path('', frontpage, name='frontpage'),
     path('cart/', cart_detail, name='cart'),
     path('contact/', contact, name='contact'),
     path('about/', about, name='about'),
     path('admin/', admin.site.urls),
+
+    # api
+
+    path('api/add_to_cart/', api_add_to_cart, name='api_add_to_cart'),
+
+    # store
+
     path('<slug:category_slug>/<slug:slug>/', product_detail, name='product_detail'),
     path('<slug:slug>/', category_detail, name='category_detail'),
 
